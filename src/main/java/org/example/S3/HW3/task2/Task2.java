b6318100d062e50812cd6139db77addbfb6cb52c
Kristina <Bolshakovakristy@gmail.com>
2023-10-26 16:49:21 +0300

    HW3

--- src/main/java/org/example/S3/HW3/task2/Task2.java
+++ src/main/java/org/example/S3/HW3/task2/Task2.java
@@ -2,14 +2,13 @@ package org.example.S3.HW3.task2;
 
 public class Task2 {
     public static <T> boolean compareArrays(T[] array1, T[] array2) {
-        if (array1.length != array2.length) {
-            return false;
-        }
-        for (int i = 0; i < array1.length; i++) {
-            if (!array1[i].equals(array2[i])) {
-                return false;
+        if(array1.length == array2.length){
+            for (int i = 0; i < array1.length; i++) {
+                if (array1[i].equals(array2[i])){
+                    return true;
+                }
             }
         }
-        return true;
+        return false;
     }
 }
